adb2b3f6fcde6a5b8694505120a899cf0671aed7
thedevak <akthedev@outlook.com>
2022-04-28 02:10:31 +0530

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch('https://sabkuchyahin.com/wp-json/wp/v2/posts/');
+  const data = await res.json();
+
+  return {
+    props: { posts: data },
+  };
+};
+
+const Home = ({ posts }) => {
+  console.log(posts);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
-      <main className={styles.main}>Homepage</main>
+    <div>
+      <h1>All Post</h1>
+      {posts.map((post) => (
+        <Link href={'/ninjas/' + post.id} key={post.id}>
+          <a className={styles.single}>
+            <img
+              src={post.jetpack_featured_media_url}
+              alt={post.title.rendered}
+              width="150px"
+            />
+            <h5>{post.title.rendered}</h5>
+          </a>
+        </Link>
+      ))}
     </div>
   );
-}
+};
+
+export default Home;
